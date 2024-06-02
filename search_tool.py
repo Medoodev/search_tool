a7e61827d5894010027a53bf7459b84da96f23bb
Medoo dev <117770788+Medoodev@users.noreply.github.com>
2024-06-03 02:33:13 +0300

    1

---
+++ search_tool.py
@@ -0,0 +1,45 @@
+import requests
+import argparse
+from bs4 import BeautifulSoup
+import socket
+from concurrent.futures import ThreadPoolExecutor
+
+p=argparse.ArgumentParser()
+p.add_argument("-u",type=str,help="example:python search_tool.py -u example.com ")
+a=p.parse_args()
+print(f"""
+
+███████╗███████╗ █████╗ ██████╗  ██████╗██╗  ██╗    ████████╗ ██████╗  ██████╗ ██╗     
+██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+███████╗█████╗  ███████║██████╔╝██║     ███████║       ██║   ██║   ██║██║   ██║██║     
+╚════██║██╔══╝  ██╔══██║██╔══██╗██║     ██╔══██║       ██║   ██║   ██║██║   ██║██║     
+███████║███████╗██║  ██║██║  ██║╚██████╗██║  ██║       ██║   ╚██████╔╝╚██████╔╝███████╗
+╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+                                                                                       
+
+      by @0xmedoo         
+
+      ------------------------------
+      target:{a.u}
+      scan:started
+      ------------------------------                                                            
+
+""")
+
+
+with open("google_dorking.txt","r") as google:
+    google_dorking_list=google.read().splitlines()
+    google.close()
+with open("fuzz.txt","r") as fuzz:
+    fuzz_list=fuzz.read().splitlines()
+    fuzz.close()
+
+
+for fuzz1 in fuzz_list:
+    try:
+        req=requests.get(f"{a.u}{fuzz1}")
+        print(f"{req.status_code}-{a.u}{fuzz1}")
+    except requests.exceptions.RequestException as e:
+        print(f"page not found- {a.u+fuzz1}")
+        print(e)
+        continue
